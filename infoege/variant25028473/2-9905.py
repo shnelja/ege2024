fe6a41c1ffc93d29e7891fc1ce7df35ab1be131d
shnelja <shnelja@gmail.com>
2024-02-16 21:16:57 +0500

    commit

--- infoege/variant25028473/2-9905.py
+++
@@ -1,8 +0,0 @@
-print('x y z w F')
-for x in 0, 1:
-    for y in 0, 1:
-        for z in 0, 1:
-            for w in 0, 1:
-                F = (not(x) or y) and (not(y) or z) and (not(z) or w)
-                if F == 1 or F == True:
-                    print(x, y, z, w, F)
